83dc9398b81ca10618a222193fc32ed54ab34858
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2025-06-03 01:39:20 +0000

    [LeetCode Sync] Runtime - 2 ms (100.00%), Memory - 63.8 MB (87.50%)

---
+++ Data Structures & Algorithms/1424-maximum-candies-you-can-get-from-boxes/solution.js
@@ -0,0 +1,19 @@
+const maxCandies = (status, candies, keys, containedBoxes, initialBoxes) => {
+    let queue = [...initialBoxes];
+    let total = 0;
+
+    while (queue.length > 0) {
+        let box = queue.shift();
+        
+        if (!status[box]) {
+            if (!queue.length) return total;
+            queue.push(box);
+        } else {
+            total += candies[box];
+            for (const key of keys[box]) status[key] = 1;
+            for (const contained of containedBoxes[box]) queue.push(contained);
+        }
+    }
+
+    return total;
+};
