bfa6f58df85fd016edc6b9d8d2d75275815841f9
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-07-02 22:38:14 +0530

    features

--- Stacks/javastack.java
+++ Stacks/javastack.java
@@ -2,11 +2,13 @@ import java.util.Stack;
 
 public class javastack{
     public static boolean isvalid(String str){
+
         Stack<Character> s = new Stack<>();
         for(int i = 0 ;i<str.length();i++){
             char ch = str.charAt(i);
             //opening
             if(ch=='(' || ch=='{' || ch=='['){
+                
                 s.push(ch);
             }else{
                 //closing
