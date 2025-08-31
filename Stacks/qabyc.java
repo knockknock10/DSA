ce95d3d265054f904caa2d50a986c53518436faa
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-08-31 21:38:32 +0530

    new question

--- Stacks/qabyc.java
+++ Stacks/qabyc.java
@@ -1,87 +1,172 @@
+import java.util.LinkedList;
 import java.util.Stack;
+import java.util.Queue;
 
-public class qabyc {   
-    public static class Queue{
-        static Stack<Integer> s1 = new Stack<>();
-        static Stack<Integer> s2 = new Stack<>();
-        public static boolean isEmpty(){
-            return s1.isEmpty();
-        }//add
-        public static void add(int data){
-            while(!s1.isEmpty()){
-                s2.push(s1.pop());
+public class qabyc {
+    //Stacks using 2 Queue using pop
+    public static class Stack {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+        static boolean isEmpty(){
+            return q1.isEmpty() && q2.isEmpty();
+        }
+        static void push(int data){
+            if(!q1.isEmpty()){
+                q1.add(data);
+            }else{
+                q2.add(data);
             }
-            s1.add(data);
-            while (!s2.isEmpty()) {
-                s1.push(s2.pop());
-            }
-        }//remove
-        public static int remove(){
-            if(s1.isEmpty()){
-                System.out.println("Empty");
+        }
+        static int pop(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
                 return -1;
             }
-            return s1.pop();
+            int top = -1;
+            //case 1
+            if(!q1.isEmpty()){
+                while(!q1.isEmpty()){
+                    top = q1.remove();
+                    if(q1.isEmpty()){
+                        break;
+                    }q2.add(top);
+                }
+                //if not last element then add to q2 else return
+            }else{//case 2
+                while(!q2.isEmpty()){
+                    top = q2.remove();
+                    if(q2.isEmpty()){
+                        break;
+                    }q1.add(top);
+                }
+                
+            }
+            return top;
         }
-        public static int peek(){
-            if(s1.isEmpty()){
-                System.out.println("Empty");
+        static int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
                 return -1;
             }
-            return s1.peek();
+            int top = -1;
+            //case 1
+            if(!q1.isEmpty()){
+                while(!q1.isEmpty()){
+                    top = q1.remove();
+                    q2.add(top);
+                }
+                //if not last element then add to q2 else return
+                
+            }else{//case 2
+                while(!q2.isEmpty()){
+                    top = q2.remove();
+                    q1.add(top);
+                }
+                
+            }
+            return top;
         }
+
+        
     }
-    //finding the largest rectangle in a histogram    // T.C  O(n)
-    public static void maxArea_histogram(int arr[]){
-        int maxarea = 0;
-        int nsr[] = new int[arr.length];
-        int nsl[] = new int[arr.length];
-        //next smaller right    O(n)
-        Stack<Integer> s = new Stack<>();
-        for(int i = arr.length-1;i>=0;i--){
-            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
-                s.pop();
-            }
-            if(s.isEmpty()){
-                nsr[i] = arr.length;
-            }else{
-                nsr[i] = s.peek();
-            }
-            s.push(i);
-        }
-        //next smaller left    O(n)
-        s = new Stack<>();
-        for(int i = 0;i<arr.length;i++){
-            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
-                s.pop();
-            }
-            if(s.isEmpty()){
-                nsl[i] = -1;
-            }else{
-                nsl[i] = s.peek();
-            }
-            s.push(i);
-        }
-        //current area : width = j-i-1 = nsr[i]-nsl[i]-1  O(n)
-        for(int i = 0;i<arr.length;i++){
-            int height = arr[i];
-            int width = nsr[i]-nsl[i]-1;
-            int currarea = height*width;
-            maxarea = Math.max(currarea,maxarea);
-        }
-        System.out.println("maxium area in histogram is "+maxarea);
-         
-    }
-    public static void main(String args[]){
+
+    //Queue using 2 stakcs using push
+    // public static class Queue {
+    //     static Stack<Integer> s1 = new Stack<>();
+    //     static Stack<Integer> s2 = new Stack<>();
+
+    //     public static boolean isEmpty() {
+    //         return s1.isEmpty();
+    //     }// add
+
+    //     public static void add(int data) {
+    //         while (!s1.isEmpty()) {
+    //             s2.push(s1.pop());
+    //         }
+    //         s1.add(data);
+    //         while (!s2.isEmpty()) {
+    //             s1.push(s2.pop());
+    //         }
+    //     }// remove
+
+    //     public static int remove() {
+    //         if (s1.isEmpty()) {
+    //             System.out.println("Empty");
+    //             return -1;
+    //         }
+    //         return s1.pop();
+    //     }
+
+    //     public static int peek() {
+    //         if (s1.isEmpty()) {
+    //             System.out.println("Empty");
+    //             return -1;
+    //         }
+    //         return s1.peek();
+    //     }
+    // }
+
+    // finding the largest rectangle in a histogram // T.C O(n)
+    // public static void maxArea_histogram(int arr[]) {
+    //     int maxarea = 0;
+    //     int nsr[] = new int[arr.length];
+    //     int nsl[] = new int[arr.length];
+    //     // next smaller right O(n)
+    //     Stack<Integer> s = new Stack<>();
+    //     for (int i = arr.length - 1; i >= 0; i--) {
+    //         while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+    //             s.pop();
+    //         }
+    //         if (s.isEmpty()) {
+    //             nsr[i] = arr.length;
+    //         } else {
+    //             nsr[i] = s.peek();
+    //         }
+    //         s.push(i);
+    //     }
+    //     // next smaller left O(n)
+    //     s = new Stack<>();
+    //     for (int i = 0; i < arr.length; i++) {
+    //         while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+    //             s.pop();
+    //         }
+    //         if (s.isEmpty()) {
+    //             nsl[i] = -1;
+    //         } else {
+    //             nsl[i] = s.peek();
+    //         }
+    //         s.push(i);
+    //     }
+    //     // current area : width = j-i-1 = nsr[i]-nsl[i]-1 O(n)
+    //     for (int i = 0; i < arr.length; i++) {
+    //         int height = arr[i];
+    //         int width = nsr[i] - nsl[i] - 1;
+    //         int currarea = height * width;
+    //         maxarea = Math.max(currarea, maxarea);
+    //     }
+    //     System.out.println("maxium area in histogram is " + maxarea);
+
+    // }
+ 
+    public static void main(String args[]) {
         // int arr[]={2,1,5,6,2,3};
         // maxArea_histogram(arr);
-        Queue q = new Queue();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        while(!q.isEmpty()){
-            System.out.println(q.peek());
-            q.remove();
+        // Queue q = new Queue();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // while (!q.isEmpty()) {
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();            
         }
+
     }
 }
