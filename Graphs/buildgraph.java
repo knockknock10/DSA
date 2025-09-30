84f27d41e4c613fd48dc0b0b06e8fb27fa08bb57
knock <116096166+knockknock10@users.noreply.github.com>
2025-09-30 11:52:23 +0530

    Update buildgraph.java

--- Graphs/buildgraph.java
+++ Graphs/buildgraph.java
@@ -2,7 +2,11 @@ package DSA.Graphs;
 
 public class buildgraph {
     Node graph{
-        
+        int data;
+        int weight;
+        Node graph(int data,int weight){
+            this.data = data;
+        }
     }
     
 }
