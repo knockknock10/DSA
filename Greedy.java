849ad3fe560431047aa93f0a4a186e9fe1d70662
knock <116096166+knockknock10@users.noreply.github.com>
2025-10-05 13:31:11 +0530

    Job seqencing problem solution

--- Greedy.java
+++ Greedy.java
@@ -1,3 +1,6 @@
+
+
+
 // Indian coins solution
     public static void indiancoins() {
         // to sort in rev order our arr should be as Integer not int
@@ -51,14 +54,54 @@ public static void minabsdi(int a[], int b[]) {
     }
 
 
+// Job Sequencing Problem
+    static class Job {
+        int deadline;
+        int profit;
+        int id;// 1(a),2(b)
 
-publice static void main(String args[]){
-    int a[]={4,1,8,7};
-    int b[]={2,3,6,5};
-    minabsdi(a,b);
-    //ans:6
-        maxhcain();//ans 3
-        indiancoins();//ans Minimum no of coins to be give is : 4
-        //500 50 20 20  
+        public Job(int i, int d, int p) {
+            id = i;
+            deadline = d;
+            profit = p;
+        }
 
-}
+    }
+
+    public static void main(String args[]) {
+        // int arr[] = { 5, 4, 3, 2, 1 };
+        // // bubble_sort(arr);
+        // selection_sort(arr);
+        // printarrr(arr);
+        // int a[] = { 4, 1, 8, 7 };
+        // int b[] = { 2, 3, 6, 5 };
+        // minabsdi(a, b);
+        // maxchain();
+        // indiancoins();
+        int jobsinfo[][] = { { 4, 20 }, { 1, 10 }, { 1, 40 }, { 1, 30 } };
+        // Job jobs[] = new Job[jobsinfo.length];
+        ArrayList<Job> jobs = new ArrayList<>();
+        for (int i = 0; i < jobsinfo.length; i++) {
+            jobs.add(new Job(i, jobsinfo[i][0], jobsinfo[i][1]));
+        }
+        Collections.sort(jobs, (obj1, obj2) -> obj2.profit - obj1.profit);// decs order
+        // for asc just Collections.sort(jobs,(a,b) -> a.profit-b.profit);
+
+        ArrayList<Integer> seq = new ArrayList<>();
+        int time = 0;
+        for (int i = 0; i < jobs.size(); i++) {
+            Job curr = jobs.get(i);
+            if (curr.deadline > time) {
+                seq.add(curr.id);
+                time++;
+            }
+        }
+        // print seq
+        System.out.println("max jobs = " + seq.size());
+        for (int i = 0; i < seq.size(); i++) {
+            System.out.print(seq.get(i) + " ");
+        }
+        System.out.println();
+        //max jobs = 2
+            //2 0 
+    }
