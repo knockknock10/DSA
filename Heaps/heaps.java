c3b2348d38c7534741f1ea4970b9bb80b6839413
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-11-12 04:07:11 +0530

    Resolved merge conflict and updated Heaps

--- Heaps/heaps.java
+++ Heaps/heaps.java
@@ -3,6 +3,61 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class heaps {
+    static class Point implements Comparable<Point> {
+        int x;
+        int y;
+        int distsq;
+        int idx;
+
+        public Point(int x, int y, int distsq, int idx) {
+            this.x = x;
+            this.y = y;
+            this.distsq = distsq;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Point p2) {
+            return this.distsq - p2.distsq;
+        }
+    }
+
+    // Heap sort
+    public static void heapify(int arr[], int i, int size) {
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        int maxIdx = i;
+        if (left < size && arr[left] > arr[maxIdx]) {
+            maxIdx = left;
+        }
+        if (right < size && arr[right] > arr[maxIdx]) {
+            maxIdx = right;
+        }
+        if (maxIdx != i) {
+            // sawp
+            int temp = arr[i];
+            arr[i] = arr[maxIdx];
+            arr[maxIdx] = temp;
+            heapify(arr, maxIdx, size);
+        }
+    }
+
+    public static void heapsort(int arr[]) {
+        // 1st to build maxheap
+        int n = arr.length;
+        for (int i = n / 2; i >= 0; i--) {
+            heapify(arr, i, n); // O(n/2*log n) i.e O(n log n)
+        }
+        // 2nd push largest at end
+        for (int i = n - 1; i > 0; i--) { // O(n)
+            // swap
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, 0, i); // O(log n) i.e O(n log n)
+        }
+    } // n logn + n logn i.e O(n log n)
+
     // Insert in heap
     static class Heap {
         ArrayList<Integer> arr = new ArrayList<>();
@@ -108,14 +163,33 @@ public class heaps {
         // pq.remove();// O(log n)
         // }
 
-        Heap h = new Heap();
-        h.add(3);
-        h.add(4);
-        h.add(1);
-        h.add(5);
-        while (!h.isEmpty()) {
-            System.out.println(h.peek());
-            h.remove();
+        // Heap h = new Heap();
+        // h.add(3);
+        // h.add(4);
+        // h.add(1);
+        // h.add(5);
+        // while (!h.isEmpty()) {
+        // System.out.println(h.peek());
+        // h.remove();
+        // }
+
+        // int arr[] = { 1, 2, 4, 5, 3 };
+        // heapsort(arr);
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.print(arr[i] + " ");
+        // }
+        // System.out.println();
+
+        int pts[][] = { { 3, 3 }, { 5, -1 }, { -2, 4 } };
+        int k = 2;
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        for (int i = 0; i < pts.length; i++) {
+            int distsq = pts[i][0] * pts[i][0] + pts[i][1] * pts[i][1];
+            pq.add(new Point(pts[i][0], pts[i][1], distsq, i));
+        }
+        // nearest k cars
+        for (int i = 0; i < k; i++) {
+            System.out.println("C" + pq.remove().idx);
         }
     }
 }
