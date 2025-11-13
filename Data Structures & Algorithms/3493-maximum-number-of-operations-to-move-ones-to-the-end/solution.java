e4211084573d8ba84a923be2b5c13ab4700d0fec
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2025-11-13 07:16:37 +0000

    [LeetCode Sync] Runtime - 7 ms (92.74%), Memory - 47.7 MB (39.52%)

---
+++ Data Structures & Algorithms/3493-maximum-number-of-operations-to-move-ones-to-the-end/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxOperations(String s) {
+        int res = 0,one =0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                one++;
+            }else if(i>0 && s.charAt(i-1)=='1'){
+                res+=one;
+            }
+        }
+        return res;
+    }
+}
