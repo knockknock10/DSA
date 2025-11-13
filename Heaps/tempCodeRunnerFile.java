52c80b86654d098dd4d94bf55e58cf84730ffee6
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-11-13 10:52:50 +0530

    Sliding Window Maximum
    Co-authored-by: Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>

---
+++ Heaps/tempCodeRunnerFile.java
@@ -0,0 +1,16 @@
+int army[][] = { { 1, 0, 0, 0 },
+        // { 1, 1, 1, 1 },
+        // { 1, 0, 0, 0 },
+        // { 1, 0, 0, 0 } };
+        // int k = 2;
+        // PriorityQueue<Row> Pq = new PriorityQueue<>();
+        // for (int i = 0; i < army.length; i++) {
+        // int count = 0;
+        // for (int j = 0; j < army[0].length; i++) {
+        // count += army[i][j] == 1 ? 1 : 0;
+        // }
+        // Pq.add(new Row(count, i));
+        // }
+        // for (int i = 0; i < k; i++) {
+        // System.out.println("R" + Pq.remove().idx);
+        // }
