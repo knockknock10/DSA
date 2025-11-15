1df92259a4a18e9437c7dfb1bf0556363fa1e782
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2025-11-15 07:21:06 +0530

    Hashing code implementation from sratch

--- Hashing/hashimp.java
+++ Hashing/hashimp.java
@@ -18,7 +18,7 @@ public class hashimp {
         private int N;
         private LinkedList<Node> buckets[]; // N = buckets.length
 
-        @SuppressWarnings("Unchecked") // if not then we have to tell
+        @SuppressWarnings("unchecked") // if not then we have to tell
         public HashMap() {
 
             this.N = 4;
@@ -46,6 +46,7 @@ public class hashimp {
             return -1;
         }
 
+        @SuppressWarnings("unchecked")
         private void rehash() {
             LinkedList<Node> oldbuck[] = buckets;
             buckets = new LinkedList[N * 2];
@@ -53,11 +54,20 @@ public class hashimp {
             for (int i = 0; i < oldbuck.length; i++) { // intialize with empty linked list
                 buckets[i] = new LinkedList<>();
             }
+            // nodes-> add in bucket
+            for (int i = 0; i < oldbuck.length; i++) {
+                LinkedList<Node> ll = oldbuck[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.remove();
+                    put(node.key, node.value);
+                }
+            }
         }
 
-        public void put(k key, v value) {
+        public void put(k key, v value) { // O(lambda)->O(1)
             int bi = hashFunction(key); // should be 0 to size-1
             int di = SearchInLL(key, bi); // valid;-1
+
             if (di != -1) {
                 Node node = buckets[bi].get(di); // bucket indx then in that ll indx
                 node.value = value;
@@ -71,21 +81,73 @@ public class hashimp {
             }
         }
 
-        public boolean containsKey(k key) {
-            return false;
+        public boolean containsKey(k key) { // O(1)
+            int bi = hashFunction(key); // should be 0 to size-1
+            int di = SearchInLL(key, bi); // valid;-1
+
+            if (di != -1) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
-        public v remove() {
-            return null;
+        public v remove(k key) { // O(1)
+            int bi = hashFunction(key); // should be 0 to size-1
+            int di = SearchInLL(key, bi); // valid;-1
+
+            if (di != -1) {
+                Node node = buckets[bi].remove(di); // bucket indx then in that ll indx
+                n--;
+                return node.value;
+            } else { // if that key doesnot exits
+                return null;
+            }
+        }
+
+        public v get(k key) { // O(1)
+            int bi = hashFunction(key); // should be 0 to size-1
+            int di = SearchInLL(key, bi); // valid;-1
+
+            if (di != -1) {
+                Node node = buckets[bi].get(di); // bucket indx then in that ll indx
+                return node.value;
+            } else { // if that key doesnot exits
+                return null;
+            }
+
         }
 
         public ArrayList<k> keySet() {
-            return null;
+            ArrayList<k> keys = new ArrayList<>();
+            for (int i = 0; i < buckets.length; i++) {
+                LinkedList<Node> ll = buckets[i];
+                for (Node node : ll) {
+                    keys.add(node.key);
+                }
+            }
+            return keys;
+        }
+
+        public boolean isEmpty() {
+            return n == 0;
         }
 
     }
 
     public static void main(String args[]) {
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("India", 40);
+        hm.put("China", 1230);
+        hm.put("Nepal", 243);
+        hm.put("us", 234);
 
+        ArrayList<String> keys = hm.keySet();
+        for (String key : keys) {
+            System.out.println(key);
+        }
+        System.out.println(hm.get("India"));
+        System.out.println(hm.remove("India"));
+        System.out.println(hm.get("India"));
     }
 }
