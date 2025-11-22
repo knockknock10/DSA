db491f58e33e4b3a91aa67fa842d02ea9cd861bc
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2025-11-22 09:46:52 +0000

    [LeetCode Sync] Runtime - 1 ms (99.88%), Memory - 55.8 MB (38.37%)

--- Data Structures & Algorithms/0169-majority-element/solution.java
+++ Data Structures & Algorithms/0169-majority-element/solution.java
@@ -1,7 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays. sort(nums);
-        int n= nums. length;
-        return nums[n/2];
+        int count =0;
+        int maj = 0;
+        for(int i:nums){
+            if(count==0){
+                count=1;
+                maj =i;
+            }else if(i==maj){
+                count++;
+            }else{
+                count--;
+            }
+        }return maj;
     }
 }
