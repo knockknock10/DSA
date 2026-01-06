2ee987713149ad28b7af69bacbf819e29bd9cf0c
knock <116096166+knockknock10@users.noreply.github.com>
2026-01-06 18:08:00 +0530

    Update Greedy.java

--- Greedy.java
+++ Greedy.java
@@ -8,7 +8,7 @@
         Arrays.sort(coins, Comparator.reverseOrder());
         int countofcoins = 0;
         int amount = 590;
-        ArrayList<Integer> ans = new ArrayList<>()
+        ArrayList<Integer> ans = new ArrayList<>();
         for (int i = 0; i < coins.length; i++) {
             if (coins[i] <= amount) {
                 while (coins[i] <= amount) {
