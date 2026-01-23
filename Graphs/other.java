1e96dbecc8a6b9fab0a4b0b26fbbe1fec19405d8
Sanjeev Kumar <karthikjaiswal10111@gmail.com>
2026-01-23 17:33:06 +0530

    Articulation Point

--- Graphs/other.java
+++ Graphs/other.java
@@ -118,6 +118,27 @@ public class other {
 
         // graph[3].add(new Edge(3, 4));
         // for Bridges in Graphs
+    //     graph[0].add(new Edge(0, 1));
+    //     graph[0].add(new Edge(0, 2));
+    //     graph[0].add(new Edge(0, 3));
+
+    //     graph[1].add(new Edge(1, 0));
+    //     graph[1].add(new Edge(1, 2));
+
+    //     graph[2].add(new Edge(2, 0));
+    //     graph[2].add(new Edge(2, 1));
+
+    //     graph[3].add(new Edge(3, 0));
+    //     graph[3].add(new Edge(3, 4));
+    //    // graph[3].add(new Edge(3, 5));
+
+    //     graph[4].add(new Edge(4, 3));
+    //     //graph[4].add(new Edge(4, 5));
+
+    //    // graph[5].add(new Edge(5, 3));
+    //    // graph[5].add(new Edge(5, 4));
+    
+    //for Articulation Point
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
         graph[0].add(new Edge(0, 3));
@@ -130,13 +151,8 @@ public class other {
 
         graph[3].add(new Edge(3, 0));
         graph[3].add(new Edge(3, 4));
-       // graph[3].add(new Edge(3, 5));
 
         graph[4].add(new Edge(4, 3));
-        //graph[4].add(new Edge(4, 5));
-
-       // graph[5].add(new Edge(5, 3));
-       // graph[5].add(new Edge(5, 4));
 
     }
 
@@ -232,6 +248,59 @@ public class other {
             }
         }
     }
+    
+    //Articultion point  Tc O(V+E)
+    public static void dffs(ArrayList<Edge> graph[],int curr,int par,int dt[],int low[],int time,boolean vis[],boolean ap[]){
+        vis[curr] = true;
+        dt[curr] = low[curr] = ++time;
+        int children = 0;
+        
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e = graph[curr].get(i);
+            int neigh = e.dest;
+            
+            if(par == neigh){
+                continue;
+            }else if(vis[neigh]){
+                low[curr] = Math.min(low[curr],dt[neigh]);
+            }else{
+                dffs(graph, neigh, curr,dt,low,time,vis,ap);
+                low[curr] = Math.min(low[curr], low[neigh]);
+                if(par!=-1 && dt[curr]<=low[neigh]){
+                    // System.out.println("Ap : "+curr);
+                    ap[curr] = true;
+                }
+                children++;
+            }
+        }
+        
+        if(par==-1 && children>1){
+            // System.out.println("Ap : "+curr);
+            ap[curr] = true;
+        }
+    }
+    public static void getAp(ArrayList<Edge> graph[],int V){
+        int dt[] = new int[V];
+        int low[] = new int[V];
+        int time = 0;
+        boolean vis[] = new boolean[V];
+        //sometimes for large it may give same ap so
+        boolean ap[] = new boolean[V];//if ap there then T
+        
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                //dfs()
+                dffs(graph, i, -1, dt, low, time, vis,ap);
+            }
+        }
+        //print all AP
+        for(int i=0;i<V;i++){
+            if(ap[i]){
+                System.out.println("Ap : "+i);
+            }
+        }
+    }
+    
     public static void main(String args[]) {
         // init();
         // System.out.println(find(3));
@@ -253,6 +322,7 @@ public class other {
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph1(graph);
         //kosaraju(graph, V);
-        Tarjans_bridge(graph,V);
+        // Tarjans_bridge(graph,V);
+        getAp(graph, V);
     }
 }
