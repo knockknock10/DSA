88e0eaf5ac9a5a96bacd9670573753b438ff2931
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-02-01 06:41:51 +0000

    [LeetCode Sync] Runtime - 1 ms (98.77%), Memory - 45.6 MB (28.40%)

---
+++ Data Structures & Algorithms/3263-divide-an-array-into-subarrays-with-minimum-cost-i/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+        
+        int min = Integer.MAX_VALUE;
+        for(int i=1;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                min = Math.min(min,nums[0]+nums[i]+nums[j]);
+            }
+        }
+        return min;
+    }
+}
