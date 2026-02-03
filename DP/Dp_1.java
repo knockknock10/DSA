4fcb22cab01e9e6cec7dff9b05cc76eab6845b30
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-02-03 21:17:47 +0530

    Knapsack Tabulation

--- DP/Dp_1.java
+++ DP/Dp_1.java
@@ -96,6 +96,31 @@ public class Dp_1{
             return dp[n][W];
         }
     }
+    public static int knapsackTab(int val[],int wt[],int W){
+        int n = val.length;
+        int dp[][] = new int[n+1][W+1];
+        for(int i=0;i<dp.length;i++){//oth col intialize
+            dp[i][0] = 0;
+        }
+        for(int j=0;j<dp[0].length;j++){ //oth row
+            dp[0][j] = 0;
+        }
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<W+1;j++){
+                int v = val[i-1];//ith item value
+                int w = wt[i-1];//ith item wt
+                if(w<=j){//valid
+                    int incProfit = v+dp[i-1][j-w];
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = Math.max(incProfit, excProfit);
+                }else{//invalid
+                    int excProfit = dp[i-1][j];
+                    dp[i][j] = excProfit;
+                }
+            }
+        }
+        return dp[n][W];
+    }
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n+1];
@@ -110,18 +135,20 @@ public class Dp_1{
         int wt[] = {2,5,1,3,4};
         int W = 7;
         int no = val.length;
-        System.out.println(knapsack(val, wt, W, no));
-        int dp[][] = new int[no+1][W+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j] = -1;
-            }
-        }
-        System.out.println(knapsackmemo(val, wt, W, no, dp));
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                System.out.print(dp[i][j]+" ");
-            }System.out.println();
-        }System.out.println();
+        // System.out.println(knapsack(val, wt, W, no));
+        // int dp[][] = new int[no+1][W+1];
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // System.out.println(knapsackmemo(val, wt, W, no, dp));
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         System.out.print(dp[i][j]+" ");
+        //     }System.out.println();
+        // }System.out.println();
+        System.out.println(knapsackTab(val, wt, W));
+        
     }
 }
