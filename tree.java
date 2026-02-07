4d3bb290d2e4d93241de45ae517035f5cd4d8c58
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-02-07 15:17:35 +0530

    Coin change problem

--- tree.java
+++ tree.java
@@ -1,19 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class tree {
     static class Node {
         int data;
         Node left;
         Node right;
-        Node(int data){
+
+        Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
         }
     }
-    static class Binarytree{
+
+    static class Binarytree {
         static int idx = -1;
-        public static Node tree(int node[]){
+
+        public static Node tree(int node[]) {
             idx++;
-            if(node[idx]==-1){
+            if (node[idx] == -1) {
                 return null;
             }
             Node newNode = new Node(node[idx]);
@@ -22,46 +28,80 @@ public class tree {
             return newNode;
         }
     }
-    public static void preorder(Node root){
-        if(root==null){
+
+    public static void preorder(Node root) {
+        if (root == null) {
             return;
         }
         System.out.print(root.data);
         preorder(root.left);
         preorder(root.right);
     }
-    public static void inorder(Node root){
-        if(root==null){
+
+    public static void inorder(Node root) {
+        if (root == null) {
             return;
         }
         inorder(root.left);
         System.out.print(root.data);
         inorder(root.right);
     }
-    public static void postorder(Node root){
-        if(root==null){
+
+    public static void postorder(Node root) {
+        if (root == null) {
             return;
         }
         postorder(root.left);
         postorder(root.right);
         System.out.print(root.data);
     }
-    public static int countNode(Node root){
-        if(root==null){
+
+    public static int countNode(Node root) {
+        if (root == null) {
             return 0;
         }
-        return countNode(root.left)+countNode(root.right)+1;
+        return countNode(root.left) + countNode(root.right) + 1;
     }
-    public static void main(String[] args) {
-        int node[]={1,2,4,-1,-1,-1,-1};
-        Node root = Binarytree.tree(node);
-        preorder(root);
-        System.out.println();
-        inorder(root);
-        System.out.println();
-        postorder(root);
-        System.err.println();
-        System.out.println(countNode(root));
+
+    public static void level_order(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
         
+        while (!q.isEmpty()) {
+            Node curr = q.remove();
+            if (curr == null) {
+                System.out.println(curr);
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(curr.data);
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int node[] = { 1, 2, 4, -1, -1, -1, -1 };
+        Node root = Binarytree.tree(node);
+        // preorder(root);
+        // System.out.println();
+        // inorder(root);
+        // System.out.println();
+        // postorder(root);
+        // System.err.println();
+        // System.out.println(countNode(root));
+        level_order(root);
+
     }
 }
