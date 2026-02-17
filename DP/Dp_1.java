337614f32f65efa5fa210fc1b00e732bb9e81dc0
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-02-17 20:02:55 +0530

    Wildcard pattern Matching

--- DP/Dp_1.java
+++ DP/Dp_1.java
@@ -404,6 +404,44 @@ public class Dp_1{
         int del= m-lcs;
         return add+del;
     }
+    //wildcard pattern matching
+    //Hard Tc O(n*m)
+    public static boolean isMatch(String s, String p){
+        int n = s.length();
+        int m = p.length();
+        
+        boolean dp[][] = new boolean[n+1][m+1];
+        
+        //intialize
+        dp[0][0] = true;
+        //pattern = " "
+        for(int i=1;i<n+1;i++){
+            dp[i][0] = false;
+        }
+        //s = ""
+        for(int j=1;j<m+1;j++){
+            if(p.charAt(j-1)=='*'){
+                dp[0][j] = dp[0][j-1];
+            }//ehere by default otehre are false so no else condition
+        }
+        
+        //bottom up
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                //case ->ith char == jth char || jth char == ?
+                if(s.charAt(i-1)==p.charAt(j-1)||p.charAt(j-1)=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }else if(p.charAt(j-1)=='*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        //string ->n,pattern -> m
+        return dp[n][m];
+        
+    }
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n+1];
@@ -468,8 +506,11 @@ public class Dp_1{
         // String word1 = "intention";
         // String word2 = "execution";
         // System.out.println(editDistance(word1, word2));  
-        String str1 = "abcdefg";
-        String str2 = "bcdg";
-        System.out.println(string_con(str1, str2));    
+        // String str1 = "abcdefg";
+        // String str2 = "bcdg";
+        // System.out.println(string_con(str1, str2)); 
+        String s = "baaabab";
+        String p = "*****ba*****ab";
+        System.out.println(isMatch(s, p));   
     }
 }
