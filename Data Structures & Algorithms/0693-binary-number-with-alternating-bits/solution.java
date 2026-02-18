b9cab00e47462cbb6859d2e4ba90cf250378afeb
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-02-18 15:12:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (71.58%)

---
+++ Data Structures & Algorithms/0693-binary-number-with-alternating-bits/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int prev = 5;   // dummy value to indicate no previous bit yet
+        
+        while(n > 0){
+            int bit = n % 2;   // get last bit
+            n /= 2;            // remove last bit
+            
+            if(prev == 5){
+                prev = bit;    // first bit, just store it
+            }else{
+                if(bit == prev) return false; // same as previous → not alternating
+                prev = bit;
+            }
+        }
+        return true;
+    }
+}
+
