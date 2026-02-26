09c2feca01b33717d9c30df108e661079d7aff14
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-02-26 10:35:40 +0530

    matrix chain multiplication Tabulation

--- DP/Dp_1.java
+++ DP/Dp_1.java
@@ -547,6 +547,37 @@ public class Dp_1{
         }
         return dp[i][j] = ans;
     }
+    public static int mcmTab(int arr[]){
+        int n = arr.length;
+        int dp[][] = new int[n][n];
+        
+        //intitalization
+        for(int i=0;i<n;i++){
+            dp[i][i]=0;
+        }
+        //bottom up
+        for(int len=2;len<=n-1;len++){
+            for(int i=1;i<=n-len;i++){
+                int j = i+len-1;//col
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k=i;k<=j-1;k++){
+                    int cost1 = dp[i][k];
+                    int cost2 = dp[k+1][j];
+                    int cost3 = arr[i-1]*arr[k]*arr[j];
+                    dp[i][j] = Math.min(dp[i][j], cost1+cost2+cost3);
+                }
+            }
+        }
+        printdptab(dp);
+        return dp[1][n-1];
+    }
+    public static void printdptab(int dp[][]){
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n+1];
@@ -629,11 +660,11 @@ public class Dp_1{
         int arr[] = {1,2,3,4,3};
         int n = arr.length;
         // System.out.println(mcm(arr, 1, n-1));
-        int dp[][] = new int[n][n];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i], -1);
-        }
-        System.out.println(mcmmemo(arr, 1 ,n-1, dp));
-    
+        //int dp[][] = new int[n][n];
+        // for(int i=0;i<n;i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // System.out.println(mcmmemo(arr, 1 ,n-1, dp));
+        System.out.println(mcmTab(arr));
     }
 }
