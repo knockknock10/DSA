857425e500d38e10b04f823be5959c2b6e37826b
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-03-01 04:54:26 +0530

    Minimum array Jumps via Tabulation

--- DP/Dp_1.java
+++ DP/Dp_1.java
@@ -604,6 +604,27 @@ public class Dp_1{
             int sum2 = sum - sum1;
             return Math.abs(sum1-sum2);
         }
+        //Min array jumps tabulation
+        public static int minJumps(int nums[]){
+            int n = nums.length;
+            int dp[] = new int[n];
+            Arrays.fill(dp,-1);
+            dp[n-1] = 0;
+            
+            for(int i=n-2;i>=0;i--){
+                int steps = nums[i];
+                int ans = Integer.MAX_VALUE;
+                for(int j=i+1;j<=i+steps && j<n ;j++){
+                    if(dp[j]!=-1){
+                        ans = Math.min(ans, dp[j]+1);
+                    }
+                }
+                if(ans!=Integer.MAX_VALUE){
+                    dp[i] = ans;
+                }
+            }
+            return dp[0];
+        }
     public static void main(String[] args) {
         // int n = 5;
         // int f[] = new int[n+1];
@@ -692,8 +713,10 @@ public class Dp_1{
         // }
         // System.out.println(mcmmemo(arr, 1 ,n-1, dp));
         //System.out.println(mcmTab(arr));
-        int num[] = {1,6,11,5};
-        System.out.println(minPartition(num));
+        // int num[] = {1,6,11,5};
+        // System.out.println(minPartition(num));
+        int nums[] = {2,3,1,1,4};
+        System.out.println(minJumps(nums));
         
     }
 }
