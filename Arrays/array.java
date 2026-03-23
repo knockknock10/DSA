16c1824fae8252a23f0ea3760b93f436b94a225f
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-03-24 04:49:22 +0530

    Binary | Rev

--- Arrays/array.java
+++ Arrays/array.java
@@ -31,9 +31,42 @@ public class array {
             }
         }System.out.println("smallest no :"+n);
     }
+    //4)binary search O(logn)
+    public static int binary(int arr[],int key){
+        int start=0;
+        int end = arr.length;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==key){
+                return mid;
+            }if(arr[mid]>key){
+                start=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }return -1;
+    }
+    //5)Reverse an array
+    public static void rev(int arr[]){
+        int first = 0;
+        int last = arr.length-1;
+        while(first<last){
+            int temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
+            first++;
+            last--;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
     public static void main(String[] args) {
         System.out.println(idx(8));
         larg();
         small();
+        int arr[] = {1,2,3,4,5,6,7};
+        System.out.println(binary(arr, 4));
+        rev(arr);
     }
 }
