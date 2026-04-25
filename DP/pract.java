5d0b21a202af076ae42b4bb4d860b383f3e058bd
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-04-25 23:02:47 +0530

    LIS 1approach

--- DP/pract.java
+++ DP/pract.java
@@ -140,6 +140,26 @@ public class pract {
         }
         return ans;
     } 
+    //Longest Increasing Subsequence 1st approach
+    public int LIS(int[] nums){
+        int[] dp = new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            dp[i] = 1;
+            for(int j=i-1;j>=0;j--){
+                if(nums[i]>nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        int lis = 0;
+        for(int i=0;i<nums.length;i++){
+            lis = Math.max(lis,dp[i]);
+        }
+        
+        return lis;
+        
+    }
     public static void main(String[] args) {
         // int nums[] = {1,2,3,1};
         // System.out.println(rob(nums));
