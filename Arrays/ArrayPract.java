4b258079983b42356a8b8b6ac2cf5e410f1d2aeb
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-05-23 10:26:45 +0530

    88. Merge Sorted Array

--- Arrays/ArrayPract.java
+++ Arrays/ArrayPract.java
@@ -13,6 +13,28 @@ public class ArrayPract {
         }
         return i+1;
     }
+    //88. Merge Sorted Array Tc (m+n) Sc O(1)
+    public static void merge(int[] nums1,int m,int[] nums2,int n ){
+        int i = m-1;
+        int j = n-1;
+        int x = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>nums2[j]){
+                nums1[x] = nums1[i];
+                i--;
+                x--;
+            }else{
+                nums1[x]= nums2[j];
+                j--;
+                x--;
+            }
+        }//for j out of bound
+        while(j>=0){
+            nums1[x] = nums2[j];
+            j--;
+            x--;
+        }
+    }
     public static void main(String[] args) {
         int[] nums = {1,1,2};
         System.out.println(removeDuplicates(nums));
