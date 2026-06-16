6554215ea0a0594c23d6efd009b3d75beb41defb
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-06-16 16:47:40 +0000

    [LeetCode Sync] Runtime - 1 ms (95.47%), Memory - 43.4 MB (52.90%)

--- Data Structures & Algorithms/0118-pascals-triangle/solution.java
+++ Data Structures & Algorithms/0118-pascals-triangle/solution.java
@@ -1,30 +1,18 @@
 class Solution {
+
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
-
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
-
-        for (int rowNum = 1; rowNum < numRows; rowNum++) {
-            List<Integer> row = new ArrayList<>();
-            List<Integer> prevRow = triangle.get(rowNum - 1);
-
-            // The first row element is always 1.
-            row.add(1);
-
-            // Each triangle element (other than the first and last of each row)
-            // is equal to the sum of the elements above-and-to-the-left and
-            // above-and-to-the-right.
-            for (int j = 1; j < rowNum; j++) {
-                row.add(prevRow.get(j - 1) + prevRow.get(j));
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> r = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0 || i==j){
+                    r.add(1);
+                }else{
+                    r.add(ans.get(i-1).get(j-1)+ ans.get(i-1).get(j));
+                }
             }
-
-            // The last row element is always 1.
-            row.add(1);
-
-            triangle.add(row);
+            ans.add(r);
         }
-
-        return triangle;
+        return ans;
     }
 }
