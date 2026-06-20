e051de728336649ba4437b2fd08a0807bfec2669
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-06-20 14:14:03 +0530

    init LinkedList A.E Merge 2 sorted lists

---
+++ Linkedlist/quest.java
@@ -0,0 +1,39 @@
+public class quest{
+    
+    public static class Node{
+        int data;
+        Node next;
+        
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    //A.E Merge two sorted linked list
+    public static Node megre(Node l1,Node l2){
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        if(l1.data<l2.data){
+            Node temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+        Node ans = l1;
+        while(l1!=null && l2!=null){
+            Node temp = null;
+            while(l1!=null && l1.data<=l2.data){
+                temp = l1;
+                l1 = l1.next;
+            }
+            temp.next = l2;
+            Node t = l1;
+            l1 = l2;
+            l2 = t;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
