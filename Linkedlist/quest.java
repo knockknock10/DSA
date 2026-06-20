b8981fc8e3e10bfdbaa8a63acc5fc6c2eaefe55f
knockknock10 <sanjeevkumar_s@srmap.edu.in>
2026-06-20 15:50:14 +0530

    init A.E intersection of two linked list

--- Linkedlist/quest.java
+++ Linkedlist/quest.java
@@ -33,6 +33,17 @@ public class quest{
         }
         return ans;
     }
+    //Intersection of two linked list 
+    public static Node interaction(Node l1,Node l2){
+        if(l1==null || l2 == null) return null;
+        Node p1 = l1, p2 = l2;
+        while(p1!=p2){
+            p1 = (p1==null) ? l2 :p1.next;
+            p2 = (p2==null) ? l1 :p2.next;
+            
+        }
+        return p1;  
+    }
     public static void main(String[] args) {
         
     }
