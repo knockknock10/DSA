676f1955f803dcceda762f0312c8eef7c33e78b5
Sanjeev Kumar <sanjeevkumar_s@srmap.edu.in>
2026-09-03 15:27:24 +0000

    [LeetCode Sync] Runtime - 5 ms (68.66%), Memory - 121.6 MB (70.66%)

---
+++ Data Structures & Algorithms/4258-construct-uniform-parity-array-ii/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int minimum = Integer.MAX_VALUE;
+        boolean allEven = true;
+
+        for (int num : nums1) {
+            minimum = Math.min(minimum, num);
+            if (num % 2 == 1) {
+                allEven = false;
+            }
+        }
+        return allEven || minimum % 2 == 1;
+    }
+}
